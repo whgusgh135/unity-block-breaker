7391e30930f4f3e8c1f0ebc62f8cf45326fa3751
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-04-09 21:19:17 +1200

    added scroe display UI

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // config params
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     void Update()
     {
@@ -19,5 +26,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore = currentScore + pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
